0031863dac0986643c370726831269a0c271c918
Samkiroko <kirokodev@gmail.com>
2020-11-03 16:28:31 +0300

    Added product page details

--- fronend/src/data.js
+++ fronend/src/data.js
@@ -1,70 +1,77 @@
-const data ={
+const data = {
   products: [
     {
       _id: '1',
-      name:'Nike Slim Shirt',
+      name: 'Nike Slim Shirt',
       category: 'Shirts',
-      image:'/images/p1.jpg',
-      price:120,
-      brand:'Nike',
+      image: '/images/p1.jpg',
+      price: 1200,
+      countInStock: 10,
+      brand: 'Nike',
       rating: 4.5,
-      numReviews:8,
-      description: 'high quality product'
+      numReviews: 10,
+      description: 'high quality product',
     },
-      {
+    {
       _id: '2',
-      name:'Nike Slim Shirt',
+      name: 'Adidas Fit Shirt',
       category: 'Shirts',
-      image:'/images/p2.jpg',
-      price:120,
-      brand:'Nike',
-      rating: 3.5,
-      numReviews:7,
-      description: 'high quality product'
-    },  {
-      _id: '3',
-      name:'puma Slim Shirt',
-      category: 'Shirts',
-      image:'/images/p3.jpg',
-      price:1200,
-       brand:'puma',
+      image: '/images/p2.jpg',
+      price: 1000,
+      countInStock: 20,
+      brand: 'Adidas',
       rating: 4.0,
-      numReviews:16,
-      description: 'high quality product'
-    },  {
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      _id: '3',
+      name: 'Lacoste Free Shirt',
+      category: 'Shirts',
+      image: '/images/p3.jpg',
+      price: 2200,
+      countInStock: 0,
+      brand: 'Lacoste',
+      rating: 4.8,
+      numReviews: 17,
+      description: 'high quality product',
+    },
+    {
       _id: '4',
-      name:'puma Slim trouser',
-      category: 'pants',
-      image:'/images/p4.jpg',
-      price:1000,
-       brand:'puma',
-      rating: 3.0,
-      numReviews:8,
-      description: 'high quality product'
-    }, 
-     {
-      _id: '5',
-      name:'puma Slim trouser',
-      category: 'pants',
-      image:'/images/p5.jpg',
-      price:1300,
-      brand:'puma',
+      name: 'Nike Slim Pant',
+      category: 'Pants',
+      image: '/images/p4.jpg',
+      price: 780,
+      countInStock: 15,
+      brand: 'Nike',
       rating: 4.5,
-      numReviews:11,
-      description: 'high quality product'
-    },  
+      numReviews: 14,
+      description: 'high quality product',
+    },
+    {
+      _id: '5',
+      name: 'Puma Slim Pant',
+      category: 'Pants',
+      image: '/images/p5.jpg',
+      price: 650,
+      countInStock: 5,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
     {
       _id: '6',
-      name:'adidas Slim trouser',
-      category: 'pants',
-      image:'/images/p6.jpg',
-      price:1400,
-       brand:'adidas',
-      rating: 5.0,
-      numReviews:14,
-      description: 'high quality product'
-    }  
-  ]
-}
-
-export default data
+      name: 'Adidas Fit Pant',
+      category: 'Pants',
+      image: '/images/p6.jpg',
+      price: 1390,
+      countInStock: 12,
+      brand: 'Adidas',
+      rating: 4.5,
+      numReviews: 15,
+      description: 'high quality product',
+    },
+  ],
+};
+export default data;
