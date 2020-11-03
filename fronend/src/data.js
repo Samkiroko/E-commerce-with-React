d6a90b61b242c79c1646a21e4a093bad134ce741
Samkiroko <kirokodev@gmail.com>
2020-11-03 11:46:03 +0300

    created react app and made contect dynamic

---
+++ fronend/src/data.js
@@ -0,0 +1,70 @@
+const data ={
+  products: [
+    {
+      _id: '1',
+      name:'Nike Slim Shirt',
+      category: 'Shirts',
+      image:'/images/p1.jpg',
+      price:120,
+      brand:'Nike',
+      rating: 4.5,
+      numReviews:8,
+      description: 'high quality product'
+    },
+      {
+      _id: '2',
+      name:'Nike Slim Shirt',
+      category: 'Shirts',
+      image:'/images/p2.jpg',
+      price:120,
+      brand:'Nike',
+      rating: 3.5,
+      numReviews:7,
+      description: 'high quality product'
+    },  {
+      _id: '3',
+      name:'puma Slim Shirt',
+      category: 'Shirts',
+      image:'/images/p3.jpg',
+      price:1200,
+       brand:'puma',
+      rating: 4.0,
+      numReviews:16,
+      description: 'high quality product'
+    },  {
+      _id: '4',
+      name:'puma Slim trouser',
+      category: 'pants',
+      image:'/images/p4.jpg',
+      price:1000,
+       brand:'puma',
+      rating: 3.0,
+      numReviews:8,
+      description: 'high quality product'
+    }, 
+     {
+      _id: '5',
+      name:'puma Slim trouser',
+      category: 'pants',
+      image:'/images/p5.jpg',
+      price:1300,
+      brand:'puma',
+      rating: 4.5,
+      numReviews:11,
+      description: 'high quality product'
+    },  
+    {
+      _id: '6',
+      name:'adidas Slim trouser',
+      category: 'pants',
+      image:'/images/p6.jpg',
+      price:1400,
+       brand:'adidas',
+      rating: 5.0,
+      numReviews:14,
+      description: 'high quality product'
+    }  
+  ]
+}
+
+export default data
